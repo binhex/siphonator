736bc57ae2c5f504e7fe9bea4352cabb65b5cc14
binhex <megalith01@gmail.com>
2023-05-11 16:16:06 +0100

    minor todo change

--- lib/siphonator/post_processing.py
+++ lib/siphonator/post_processing.py
@@ -1,6 +1,6 @@
 # TODO read in list of torrents with category named 'movies-siphonator'
-# TODO read download location for torrents
 # TODO read filenames from torrents
+# TODO read completed path from torrent client
 # TODO strip out crappy files
 # TODO rename to imdb title
 # TODO move completed folder to media
