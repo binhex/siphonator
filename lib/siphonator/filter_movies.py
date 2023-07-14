9b60f8a107f7738fe8eb9db282a5a6572028316f
binhex <megalith01@gmail.com>
2023-07-14 11:38:38 +0100

    todo removal

--- lib/siphonator/filter_movies.py
+++ lib/siphonator/filter_movies.py
@@ -6,7 +6,6 @@ import lib.siphonator.tools_various as siphonator_tools_various
 # TODO if remastered or extended or directors cut then check for on disk, if on disk then check size, if larger then download
 # TODO filter_override_downloaded - if repack or proper then check for on disk, if there then possible override
 # TODO bad group list, start with SLOT?
-# TODO if in completed then do not download, or in qbittorrent queue, or in qbittorrent history (possible?)
 # TODO if size of movie is larger on disk compared to index title then ignore, UNLESS its remastered, proper etc.
 
 
