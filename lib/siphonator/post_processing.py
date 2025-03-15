11a541318450d9d9e1d0e7169c71f38075234eb2
binhex <megalith01@gmail.com>
2025-03-15 18:39:08 +0000

    more work on post processing

--- lib/siphonator/post_processing.py
+++ lib/siphonator/post_processing.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import lib.siphonator.torrent_clients as torrent_clients
 import lib.siphonator.db_sqlite as db_sqlite
 
@@ -32,9 +33,6 @@ class PostProcess(object):
             # rename completed files
             self.rename_completed_files(torrent_completed_dict)
 
-            # move completed files
-            self.move_completed_files(torrent_completed_dict)
-
     def remove_completed_torrents(self, torrent_completed_dict):
 
         if not self.config_dict['post_process']['remove_completed']:
@@ -51,20 +49,26 @@ class PostProcess(object):
         torrent_file_dict_list = torrent_completed_dict.get('torrent_file_list')
         torrent_save_path = torrent_completed_dict.get('torrent_save_path')
 
-        delete_files_less_than_kb = self.config_dict['post_process']['delete_files_less_than_kb']
         delete_file_ext_list = self.config_dict['post_process']['delete_file_ext_list']
+        delete_files_less_than_kb = self.config_dict['post_process']['delete_files_less_than_kb']
 
         def delete_file(path):
 
-            if os.path.isfile(path):
-                try:
-                    os.remove(path)
-                    self.logger_instance.info(f"Successfully deleted file '{path}'")
-                except OSError as e:
+            if not os.path.isfile(path):
 
-                    self.logger_instance.info(f"Failed to delete file from path '{path}', error is '{e.strerror}'")
-            else:
                 self.logger_instance.info(f"Failed to delete file from path '{path}' as the file does not exist, if running Siphonator in a Docker container ensure the Docker bind mounts for the 'Default save path' match qBittorrent")
+
+            try:
+                os.remove(path)
+                self.logger_instance.info(f"Successfully deleted file '{path}'")
+            except FileNotFoundError as e:
+                self.logger_instance.warning(f"The file '{path}' does not exist, if running Siphonator in a Docker container ensure the Docker bind mounts for qBittorrent 'Default save path' match for this container, error is '{e}'")
+            except PermissionError as e:
+                self.logger_instance.warning(f"Permission denied while trying to delete '{path}', error is '{e}'")
+            except IsADirectoryError as e:
+                self.logger_instance.warning(f"'{path}' is a directory, not a file, error is '{e}'")
+            except OSError as e:
+                self.logger_instance.warning(f"General OS error, error is '{e}'")
 
         # iterate over list containing dictionary of files in the torrent
         for torrent_file_dict in torrent_file_dict_list:
@@ -84,6 +88,7 @@ class PostProcess(object):
                     # check config delete extension matches file extension in torrent file path
                     if delete_file_ext == torrent_file_ext:
 
+                        self.logger_instance.info(f"file extension '{torrent_file_ext}' for torrent completed filepath '{torrent_file_path}' matches delete file extension '{delete_file_ext}' from config, deleting file...")
                         delete_file(torrent_file_path)
 
             if delete_files_less_than_kb:
@@ -96,8 +101,7 @@ class PostProcess(object):
                 # if torrent file_size is less than minimum size defined in config then delete
                 if int(torrent_file_size_kb) < int(delete_files_less_than_kb):
 
-                    self.logger_instance.info(f"file size {torrent_file_size_kb}KB for filepath '{torrent_file_path}' is less than minimum file size {delete_files_less_than_kb}KB defined in config file, deleting file...")
-
+                    self.logger_instance.info(f"file size {torrent_file_size_kb}KB for torrent completed filepath '{torrent_file_path}' is less than minimum file size {delete_files_less_than_kb}KB defined in config file, deleting file...")
                     delete_file(torrent_file_path)
 
     def rename_completed_files(self, torrent_completed_dict):
@@ -115,15 +119,43 @@ class PostProcess(object):
             imdb_year = query_result.get('imdb_year')
             imdb_title_year = f"{imdb_title} ({imdb_year})"
 
-        # get filepath for main video file from torrent details
+        # TODO WIP
+        # get parent folder for largest file in the torrent
+
+        # if parent folder is /data/completed then create imdb folder and move all files for torrent into folder (some may fail as they have been deleted).
 
         # create folder imdb_title_year if the torrent main video file is not in a folder and movie it there
 
         # if folder does exist and main video file is in there then rename it to imdb_title_year
 
-    def move_completed_files(self, torrent_completed_dict):
+        # call move function, passing in paths
+        # TODO WIP
+        #self.move_completed_directory(torrent_completed_dict)
+
+    def move_completed_directory(self, absolute_completed_path, movie_directory):
 
         if not self.config_dict['post_process']['move_completed']:
             return False
 
-        # move folder from completed to path defined in config
+        move_dest_path = self.config_dict['post_process']['move_dest_path']
+        if not move_dest_path:
+            return False
+
+        # check move destination path exists
+        if not os.path.isdir(move_dest_path):
+
+            self.logger_instance.warning(f"Destination path '{move_dest_path}' does not exist, if running Siphonator in a Docker container ensure the Docker bind mounts for qBittorrent 'Default save path' match for this container")
+
+        absolute_dst_path = os.path.join(move_dest_path, movie_directory)
+
+        try:
+            shutil.move(absolute_completed_path, str(absolute_dst_path))
+            self.logger_instance.info(f"Successfully moved source path '{absolute_completed_path}' to destination path '{absolute_dst_path}'")
+        except FileNotFoundError as e:
+            self.logger_instance.warning(f"The source directory '{absolute_completed_path}' does not exist if running Siphonator in a Docker container ensure the Docker bind mounts for qBittorrent 'Default save path' match for this container, error is '{e}'")
+        except PermissionError as e:
+            self.logger_instance.warning(f"Permission denied while moving '{absolute_completed_path}' to '{absolute_dst_path}', error is '{e}'")
+        except shutil.Error as e:
+            self.logger_instance.warning(f"General error, error is '{e}'")
+        except OSError as e:
+            self.logger_instance.warning(f"OS error, error is '{e}'")
